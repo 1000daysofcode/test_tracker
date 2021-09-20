0d033873158981f3c3523d8da69cbfc7be453193
Zach Frank <zachariahjfrank@gmail.com>
2021-09-20 15:35:38 +0700

    Add commented outline

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
+# Greet user
 
+# Ask for name (add to variable)
+
+# Ask for test name, then
+
+# Check number of subfolders needed by asking:
+    # "Is this divided into parts?" 
+    # Y = name part, specify number of parts, 
+    # N = Begin to get max scores for all sections
+
+# Print table organized and ask user to confirm
+
+    # Here is your test organization:
+    
+    # Test Name: 'IELTS'
+    # =============================
+    # | Test 1 | Reading | Part 1 |
+    # | Test 1 | Reading | Part 2 |
+    # | Test 1 | Reading | Part 3 |
+    # | Test 1 | Reading | Part 4 |
+    # | Test 1 | Writing | Part 1 |
+    # | Test 1 | Writing | Part 2 |
+    # | Test 1 | Writing | Part 3 |
+    # =============================
+    # + 3 more tests like 'Test 1'
+
+    # Is this correct? Y/ N
+
+# Once complete, request user to input information
+
+# Once data completed, allow user 4 options:
+    # View score trends -> Type 'S'
+    # View entire test --> Type 'T'
+    # Update test score -> Type 'U'
+    # Add another test --> Type 'A'
+
+# Type 'QUIT NOW' at any time to quit program. Confirmation y/n
