68a88d1f4c86c325e375151d2d3d0f8d54f7fe72
Zach Frank <zachariahjfrank@gmail.com>
2021-09-21 22:02:01 +0700

    Update error checking and format terminal UI for user

--- dict_create_custom.py
+++ dict_create_custom.py
@@ -11,42 +11,145 @@ def builddict():
     dict1 = {}
     dict2 = {}
     dict3 = {}
-    while moreLevels != False and moreLevels != 'n':
-        if moreLevels == 'y' or moreLevels == 'undefined':
-            moreLevels = input('Is this divided into parts? y/n ')
-            if moreLevels == 'y'.lower():
+    while moreLevels != False and moreLevels[0] != 'n':
+        if moreLevels == 'undefined':
+            while True:
+                try:
+                    moreLevels = (input('\n-----------------\nIs this divided into parts? Y/N ')).lower().strip()
+                    if moreLevels[0] == 'y' or moreLevels[0] == 'n':
+                        break
+                    elif isinstance(moreLevels, str) == False:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
+                    else:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
+                except IndexError:
+                    print('\n=================================\n| That is not a valid response. |\n=================================')
+                    continue
+            if moreLevels[0] == 'y':
                 numLevels += 1
-                get_subSize1 = int(input('How many parts? '))
+                while True:
+                    try:
+                        get_subSize1 = int(input('\nHow many identical parts? '))
+                        print('\n')
+                        if get_subSize1 > 10 or get_subSize1 < 1:
+                            print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                            continue
+                        else:
+                            break
+                    except ValueError:
+                        print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                        continue
                 for i in range(get_subSize1):
-                    subName1 = input('Please enter the section name: ')
-                    sectCount1 = int(input(f'Please enter the number of sections in {subName1}: '))
+                    while True:
+                        subName1 = input(f'Please enter a section name for section {i+1}: ')
+                        print('\n')
+                        if len(subName1) > 20 or len(subName1) <= 2:
+                            print('\n==================================================\n| Please enter a name from 3-20 characters long. |\n==================================================\n')
+                            continue
+                        else:
+                            break
+                    while True:
+                        try:
+                            sectCount1 = int(input(f'Please enter the number of sections in {subName1}: '))
+                            print('\n')
+                            if sectCount1 > 10 or sectCount1 < 1:
+                                print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                                continue
+                            else:
+                                break
+                        except ValueError:
+                            print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                            continue
                     subfold1.append([sectCount1, subName1])
-                moreLevels = input('Do you need to divide into more sections? y/n ')
+                while True:
+                    try:
+                        moreLevels = (input('-----------------\nDo you need to divide into more sections [Ask #2]? Y/N ')).lower().strip()
+                        if moreLevels[0] == 'y' or moreLevels[0] == 'n':
+                            break
+                        elif isinstance(moreLevels, str) == False:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                        else:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                    except IndexError:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
+            else:
+                dict = {'test_name':[0,'mistakes']}
+                return dict, numLevels
 
             # If yes, continue building subfolders, if no process dict
-            if moreLevels == 'y':
+            if moreLevels[0] == 'y':
                 numLevels += 1
                 # get_subSize2 = int(input('How many identical parts? '))
                 for i in subfold1:
-                    print(f'Please input for section {i[1]}')
+                    print(f'\nPlease enter the names for section {i[1]}')
                     for j in range(i[0]):
-                        subName2 = input('Please enter the section name: ')
-                        sectCount2 = int(input(f'Please enter the number of sections in {subName2}: '))
+                        while True:
+                            subName2 = input(f'Please enter the section name for section {j+1}: ')
+                            if len(subName2) > 20 or len(subName2) <= 2:
+                                print('\n==================================================\n| Please enter a name from 3-20 characters long. |\n==================================================\n')
+                                continue
+                            else:
+                                break
+                        while True:
+                            try:
+                                sectCount2 = int(input(f'Please enter the number of sections in {subName2}: '))
+                                if sectCount2 > 10 or sectCount2 < 1:
+                                    print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                                    continue
+                                else:
+                                    break
+                            except ValueError:
+                                print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                                continue
                         subfold2.append([sectCount2, subName2])           
-                moreLevels = input('Do you need to divide into more sections 2? y/n ')
+                while True:
+                    try:
+                        moreLevels = (input('\n-----------------\nDo you need to divide into more sections [Ask #3]? Y/N ')).lower().strip()
+                        if moreLevels[0] == 'y' or moreLevels[0] == 'n':
+                            break
+                        elif isinstance(moreLevels, str) == False:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                        else:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                    except IndexError:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
             else:
                 for layer1 in subfold1:
                     dict1.update({layer1[1]:[0, '']})
-                return dict1
+                return dict1, numLevels
             
             if moreLevels == 'y':
                 numLevels += 1
                 # get_subSize3 = int(input('How many identical parts? '))
                 for i in subfold2:
-                    print(f'Please input for section {i[1]}')
+                    print(f'\nPlease enter the names for section {i[1]}\n-----------------\n')
                     for j in range(i[0]):
-                        subName3 = input('Please enter the section name: ')
-                        # sectCount3 = int(input(f'Please enter the number of sections in {subName3}: '))
+                        while True:
+                            subName3 = input(f'Please enter the section name for section {j+1}: ')
+                            if len(subName3) > 20 or len(subName3) <= 2:
+                                print('\n==================================================\n| Please enter a name from 3-20 characters long. |\n==================================================\n')
+                                continue
+                            else:
+                                break
+                        while True:
+                            try:
+                                sectCount3 = int(input(f'Please enter the number of sections in {subName3}: '))
+                                if sectCount3 > 10 or sectCount3 < 1:
+                                    print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                                    continue
+                                else:
+                                    break
+                            except ValueError:
+                                print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                                continue
                         subfold3.append([sectCount3, subName3])
                 for layer3 in subfold3:
                     dict3.update({layer3[1]:[0, '']})
@@ -54,29 +157,18 @@ def builddict():
                     dict2.update({layer2[1]:dict3})
                 for layer1 in subfold1:
                     dict1.update({layer1[1]:dict2})
-                return dict1
+                return dict1, numLevels
             else:
                 for layer2 in subfold2:
                     dict2.update({layer2[1]:[0, '']})
                 for layer1 in subfold1:
                     dict1.update({layer1[1]:dict2})
-                return dict1
-        elif moreLevels == 'n'.lower():
-            pass
+                return dict1, numLevels
+        elif moreLevels == 'n':
+            dict = {'test_name':[0,'mistakes']}
+            return dict, numLevels
         else:
             continue #enclose in loop to force at least 1 y/n answer
 
-print(builddict())
-
-            #Create subfolders, for each subfolder how many fucking folders do you have and what are their names
-
-            # if numLevels > 3:
-            #                 print("You've reached the maximum number of subsections")
-            #                 levelCheck = False
-            #             else:
-            #                 get_subSize = input('How many parts?')
-            #                 for count, i in enumerate(range(get_subSize)):
-            #                     subName = input('Please enter the section name: ')
-            #                     subSize.append([count,subName])
-            #                 numLevels += 1
-            #                 continue
+dict, levels = builddict()
+print(f'\n-----------------\n\nNumber of levels: {levels}\n\nDictionary:\n\n{dict}\n')
--- dict_create_simple.py
+++ dict_create_simple.py
@@ -1,4 +1,4 @@
-def builddict():
+def builddict(testname):
     moreLevels = 'undefined'    
     maxScore = 0
     numLevels = 0
@@ -8,35 +8,137 @@ def builddict():
     dict1 = {}
     dict2 = {}
     dict3 = {}
-    while moreLevels != False and moreLevels != 'n':
-        if moreLevels == 'y' or moreLevels == 'undefined':
-            moreLevels = input('Is this divided into parts? y/n')
-            if moreLevels == 'y'.lower():
+    while moreLevels != False and moreLevels[0] != 'n':
+        if moreLevels == 'undefined':
+            while True:
+                try:
+                    moreLevels = (input('\n-----------------\nIs this divided into parts? Y/N ')).lower().strip()
+                    if moreLevels[0] == 'y' or moreLevels[0] == 'n':
+                        break
+                    elif isinstance(moreLevels, str) == False:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
+                    else:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
+                except IndexError:
+                    print('\n=================================\n| That is not a valid response. |\n=================================')
+                    continue
+            if moreLevels[0] == 'y':
                 numLevels += 1
-                get_subSize1 = int(input('How many identical parts? '))
+                while True:
+                    try:
+                        get_subSize1 = int(input('\nHow many identical parts? '))
+                        print('\n')
+                        if get_subSize1 > 10 or get_subSize1 < 1:
+                            print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                            continue
+                        else:
+                            break
+                    except ValueError:
+                        print('\n===================================\n| Please enter a number from 1-10. |\n====================================')
+                        continue
+                if get_subSize1 > 1:
+                    print(f'\nPlease enter the names for all {get_subSize1} parts:\n-----------------')
+                else: 
+                    print(f'\nPlease enter the name:\n-----------------')
                 for i in range(get_subSize1):
-                    subName1 = input('Please enter the section name: ')
+                    while True:
+                        subName1 = input(f'Please enter a section name for section {i+1}: ')
+                        if len(subName1) > 20 or len(subName1) <= 2:
+                            print('\n==================================================\n| Please enter a name from 3-20 characters long. |\n==================================================\n')
+                            continue
+                        else:
+                            break
                     subfold1.append([i+1, subName1])
-                moreLevels = input('Do you need to divide into more sections 1? y/n')
+                while True:
+                    try:
+                        moreLevels = (input('\n-----------------\nDo you need to divide into more sections [Ask #2]? Y/N ')).lower().strip()
+                        if moreLevels[0] == 'y' or moreLevels[0] == 'n':
+                            break
+                        elif isinstance(moreLevels, str) == False:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                        else:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                    except IndexError:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
+            else:
+                dict = {testname:[0,'mistakes']}
+                return dict, numLevels
 
             # If yes, continue building subfolders, if no process dict
-            if moreLevels == 'y':
+            if moreLevels[0] == 'y':
                 numLevels += 1
-                get_subSize2 = int(input('How many identical parts? '))
+                while True:
+                    try:
+                        get_subSize2 = int(input('\nHow many identical parts is this broken into? '))
+                        if get_subSize2 > 10 or get_subSize2 < 1:
+                            print('\n====================================\n| Please enter a number from 1-10. |\n====================================')
+                            continue
+                        else:
+                            break
+                    except ValueError:
+                        print('\n====================================\n| Please enter a number from 1-10. |\n====================================')
+                        continue
+                if get_subSize2 > 1:
+                    print(f'\nPlease enter the names for all {get_subSize2} parts:\n-----------------')
+                else: 
+                    print(f'\nPlease enter the name:\n-----------------')
                 for i in range(get_subSize2):
-                    subName2 = input('Please enter the section name: ')
+                    while True:
+                        subName2 = input(f'Please enter a section name for section {i+1}: ')
+                        if len(subName2) > 20 or len(subName2) <= 2:
+                            print('\n==================================================\n| Please enter a name from 3-20 characters long. |\n==================================================\n')
+                            continue
+                        else:
+                            break
                     subfold2.append([i+1, subName2])
-                moreLevels = input('Do you need to divide into more sections 2? y/n')
+                while True:
+                    try: 
+                        moreLevels = (input('\n-----------------\nDo you need to divide into more sections [Ask #3]? Y/N ')).lower().strip()
+                        if moreLevels[0] == 'y' or moreLevels[0] == 'n':
+                            break
+                        elif isinstance(moreLevels, str) == False:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                        else:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                    except IndexError:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
             else:
                 for layer1 in subfold1:
                     dict1.update({layer1[1]:[0, '']})
-                return dict1
+                return dict1, numLevels
             
-            if moreLevels == 'y':
+            if moreLevels[0] == 'y':
                 numLevels += 1
-                get_subSize3 = int(input('How many identical parts? '))
+                while True:
+                    try:
+                        get_subSize3 = int(input('\nHow many identical parts? '))
+                        if get_subSize3 > 10 or get_subSize3 < 1:
+                            print('\n====================================\n| Please enter a number from 1-10. |\n====================================')
+                            continue
+                        else:
+                            break
+                    except ValueError:
+                        print('\n====================================\n| Please enter a number from 1-10. |\n====================================')
+                if get_subSize3 > 1:
+                    print(f'\nPlease enter the names for all {get_subSize3} parts:\n-----------------')
+                else: 
+                    print(f'\nPlease enter the name:\n-----------------')
                 for i in range(get_subSize3):
-                    subName3 = input('Please enter the section name: ')
+                    while True:
+                        subName3 = input(f'Please enter a section name for section {i+1}: ')
+                        if len(subName3) > 20 or len(subName3) <= 2:
+                            print('\n==================================================\n| Please enter a name from 3-20 characters long. |\n==================================================\n')
+                            continue
+                        else:
+                            break
                     subfold3.append([i+1, subName3])
                 for layer3 in subfold3:
                     dict3.update({layer3[1]:[0, '']})
@@ -44,29 +146,18 @@ def builddict():
                     dict2.update({layer2[1]:dict3})
                 for layer1 in subfold1:
                     dict1.update({layer1[1]:dict2})
-                return dict1
+                return dict1, numLevels
             else:
                 for layer2 in subfold2:
                     dict2.update({layer2[1]:[0, '']})
                 for layer1 in subfold1:
                     dict1.update({layer1[1]:dict2})
-                return dict1
-        elif moreLevels == 'n'.lower():
-            pass
+                return dict1, numLevels
+        elif moreLevels[0] == 'n':
+            dict = {testname:[0,'mistakes']}
+            return dict, numLevels
         else:
-            continue #enclose in loop to force at least 1 y/n answer
+            continue
 
-print(builddict())
-
-            #Create subfolders, for each subfolder how many fucking folders do you have and what are their names
-
-            # if numLevels > 3:
-            #                 print("You've reached the maximum number of subsections")
-            #                 levelCheck = False
-            #             else:
-            #                 get_subSize = input('How many parts?')
-            #                 for count, i in enumerate(range(get_subSize)):
-            #                     subName = input('Please enter the section name: ')
-            #                     subSize.append([count,subName])
-            #                 numLevels += 1
-            #                 continue
+dict, levels = builddict('IELTS')
+print(f'\n-----------------\n\nNumber of levels: {levels}\n\nDictionary:\n\n{dict}\n')
