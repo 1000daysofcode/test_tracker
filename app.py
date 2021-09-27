a9fa3d0139bf935b087eff9a1a01bd23e537ba6a
Zach Frank <zachariahjfrank@gmail.com>
2021-09-27 23:15:06 +0700

    Adjust mistake list function to force input of at least 1 mistake type

--- app.py
+++ app.py
@@ -1021,49 +1021,33 @@ Please choose S/C: ''')).lower().strip()
 # MAKE A MISTAKE LIST FOR INDEX
 def make_mlist():
     mistake_list = []
-    add_more = ''
+    add_more = 'y'
     quit = False
-
     mistakes_done = False
-
+    
     while quit == False:
-        try:
-            add_more = input('\nWould you like to add a mistake type? Y/N ').lower().strip()
-            if add_more[0] == 'y':
-                while add_more[0] == 'y':
-                    new_mistake = input('---\nEnter the name of your mistake type: ')
-                    mistake_list.append(new_mistake)
-                    while True:
-                        try:
-                            add_more = input('---\nWould you like to add more mistakes? Y/N ').lower().strip()
-                            if add_more[0] == 'y':
-                                break
-                            elif add_more[0] == 'n':
-                                quit = True
-                                mistakes_done = True
-                                break
-                            elif isinstance(add_more, str) == False:
-                                print('\n=================================\n| That is not a valid response. |\n=================================')
-                                continue
-                            else:
-                                print('\n=================================\n| That is not a valid response. |\n=================================')
-                                continue
-                        except IndexError:
+        if add_more[0] == 'y':
+            while add_more[0] == 'y':
+                new_mistake = input('---\nPlease enter one type of mistake: ')
+                mistake_list.append(new_mistake)
+                while True:
+                    try:
+                        add_more = input('---\nWould you like to add more mistakes? Y/N ').lower().strip()
+                        if add_more[0] == 'y':
+                            break
+                        elif add_more[0] == 'n':
+                            quit = True
+                            mistakes_done = True
+                            break
+                        elif isinstance(add_more, str) == False:
                             print('\n=================================\n| That is not a valid response. |\n=================================')
                             continue
-            elif add_more[0] == 'n':
-                quit = True
-                mistakes_done = False
-                break
-            elif isinstance(add_more, str) == False:
-                print('\n=================================\n| That is not a valid response. |\n=================================')
-                continue
-            else:
-                print('\n=================================\n| That is not a valid response. |\n=================================')
-                continue
-        except IndexError:
-            print('\n=================================\n| That is not a valid response. |\n=================================')
-            continue
+                        else:
+                            print('\n=================================\n| That is not a valid response. |\n=================================')
+                            continue
+                    except IndexError:
+                        print('\n=================================\n| That is not a valid response. |\n=================================')
+                        continue
     print('\nThe mistakes list is: ')
     for c, i in enumerate(mistake_list):
         print(f'Mistake {c+1}: {i}')
